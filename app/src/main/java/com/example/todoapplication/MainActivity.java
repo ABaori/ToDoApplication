a43c27957b416a66369b587ae77a5e2193bb6737
Aarav Baori <aaravbaori2@gmail.com>
2023-06-19 13:50:43 +0530

    Second Commit

--- app/src/main/java/com/example/todoapplication/MainActivity.java
+++ app/src/main/java/com/example/todoapplication/MainActivity.java
@@ -4,21 +4,27 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.concurrent.ExecutorService;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     Button button;
     EditText Title , Note;
     Context context = MainActivity.this;
-    Drawable drawable;
-    ImageView img, img2;
+
+    ImageView img;
+    Runnable runnable;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +35,40 @@ public class MainActivity extends AppCompatActivity {
         Title = findViewById(R.id.Title_edt);
         Note = findViewById(R.id.Note_edt);
         img = findViewById(R.id.Img1);
-        img2 = findViewById(R.id.Img2);
-        drawable = getDrawable(R.drawable.animation);
+        handler = new Handler();
+
+
 //        thread = new Thread();
 
 
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                img2.setVisibility(View.VISIBLE);
-                
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+
+                        img.animate().scaleX(4f).scaleY(4f).alpha(0f).setDuration(1000)
+                                .withEndAction(new Runnable() {
+                                    @Override
+                                    public void run() {
+
+                                        img.setImageResource(R.drawable.btn_anmt_bg);
+                                        Log.i(String.valueOf(context), "Inside runnable");
+
+                                    }
+
+                                });
+                    }
+
+                };
+                runnable.run();
+
+
 
             }
         });
+
 
 
 
